e298768220cee61afe29b601b5547cabd65d68de
Faiyaz Mujawar <mujawar.faiyaz.aziz@gmail.com>
2022-01-11 11:32:32 +0530

    implemented get all issues
    In this commit,
    - implemented get all issues
    - implemented get issue by id
    - implemented get comments

--- src/app/services/issues/issues.service.ts
+++ src/app/services/issues/issues.service.ts
@@ -1,6 +1,8 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import Issues from '../../scratchpad/issues.json';
+import { firstValueFrom, Observable, of } from 'rxjs';
+import { environment as ENV } from '../../../environments/environment';
+import { AuthService } from '../auth/auth.service';
 
 export interface Issue {
   description: string;
@@ -8,42 +10,113 @@ export interface Issue {
   isEdited: boolean;
   isResolved: boolean;
   isInappropriate: boolean;
-  isDeleted: boolean;
-  upvoters: string[];
-  reporters: any[];
-  createdOn: string;
+  createdOn: Date;
   _id: string;
   title: string;
   section: string;
   department: string;
   scope: string;
   upvotes: number;
-  comments: {
-    postedOn: string;
-    isInappropriate: boolean;
-    _id: string;
-    comment: string;
-    postedBy: string;
-  }[];
-  solutions: any[];
-  createdBy: string;
-  __v: number;
+  commentsCount: number;
+  solutionsCount: number;
+  isAuthor: boolean;
+  hasUpvoted: boolean;
+  hasReported: boolean;
+}
+
+export interface Comment {
+  postedOn: Date;
+  isInappropriate: boolean;
+  _id: string;
+  comment: string;
+  postedBy: string;
+}
+
+export interface Issues {
+  issues: Issue[];
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+
+export interface Comments {
+  comments: Comment[];
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssuesService {
-  private issues: Issue[] = Issues;
-  constructor() {}
+  private issues: Issue[] = [];
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
-  getAllIssues(department: string): Observable<Issue[]> {
-    return of<Issue[]>(
-      this.issues.filter((issue) => issue.department === department)
+  async getAllIssues(): Promise<Issues> {
+    const token: string = (await this.authService.getAccessToken()) as string;
+
+    const response: HttpResponse<any> = await firstValueFrom(
+      this.http.get<any>(`${ENV.API_SERVER_URI}/issues/all`, {
+        observe: 'response',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          limit: 5,
+        },
+      })
     );
+
+    const body: any = response.body;
+
+    if (response.status !== 200) {
+      throw new Error(body.message);
+    }
+
+    return body.data;
   }
 
-  getIssueById(id: string): Observable<Issue | undefined> {
-    return of<Issue | undefined>(this.issues.find((issue) => issue._id === id));
+  async getIssueById(id: string): Promise<Issue | undefined> {
+    const token: string = (await this.authService.getAccessToken()) as string;
+
+    const response: HttpResponse<any> = await firstValueFrom(
+      this.http.get<any>(`${ENV.API_SERVER_URI}/issues/${id}`, {
+        observe: 'response',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    );
+
+    const body: any = response.body;
+
+    if (response.status !== 200) {
+      throw new Error(body.message);
+    }
+
+    return body.issue;
+  }
+
+  async getComments(id: string): Promise<Comments> {
+    const token: string = (await this.authService.getAccessToken()) as string;
+
+    const response: HttpResponse<any> = await firstValueFrom(
+      this.http.get<any>(`${ENV.API_SERVER_URI}/issues/${id}/comments`, {
+        observe: 'response',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          limit: 5,
+        },
+      })
+    );
+
+    const body: any = response.body;
+
+    if (response.status !== 200) {
+      throw new Error(body.message);
+    }
+
+    return body.data;
   }
 }
